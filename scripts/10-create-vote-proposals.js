3b7b98d9da6c4f17f693135cb85e85959fc18ead
mmednik <mmednik@gmail.com>
2022-02-25 15:38:16 -0300

    create vote proposals

---
+++ scripts/10-create-vote-proposals.js
@@ -0,0 +1,61 @@
+import { ethers } from "ethers";
+import sdk from "./1-initialize-sdk.js";
+
+const voteModule = sdk.getVoteModule("0x192b690F7b2a1cE661a9606EfD08a812b32AfB5b");
+const tokenModule = sdk.getTokenModule("0x6bA18655Fd64673C76e6748c6251723ac9C0d405");
+
+(async () => {
+  try {
+    const amount = 420000;
+    await voteModule.propose(
+      "Should the DAO mint an additional " + amount + " tokens into the treasury?",
+      [
+        {
+          nativeTokenValue: 0,
+          transactionData: tokenModule.contract.interface.encodeFunctionData(
+            "mint",
+            [
+              voteModule.address,
+              ethers.utils.parseUnits(amount.toString(), 18),
+            ]
+          ),
+          toAddress: tokenModule.address,
+        },
+      ]
+    );
+
+    console.log("✅ Successfully created proposal to mint tokens");
+  } catch (error) {
+    console.error("failed to create first proposal", error);
+    process.exit(1);
+  }
+
+  try {
+    const amount = 6900;
+    await voteModule.propose(
+      "Should the DAO transfer " +
+      amount + " tokens from the treasury to " +
+      process.env.WALLET_ADDRESS + " for being awesome?",
+      [
+        {
+          nativeTokenValue: 0,
+          transactionData: tokenModule.contract.interface.encodeFunctionData(
+            "transfer",
+            [
+              process.env.WALLET_ADDRESS,
+              ethers.utils.parseUnits(amount.toString(), 18),
+            ]
+          ),
+
+          toAddress: tokenModule.address,
+        },
+      ]
+    );
+
+    console.log(
+      "✅ Successfully created proposal to reward ourselves from the treasury, let's hope people vote for it!"
+    );
+  } catch (error) {
+    console.error("failed to create second proposal", error);
+  }
+})();
