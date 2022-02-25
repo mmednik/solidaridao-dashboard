9a11a3f60bc1fe68e3f281f6b564d71cd64bd13e
mmednik <mmednik@gmail.com>
2022-02-25 10:41:19 -0300

    show members

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-
+import { ethers } from "ethers";
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 
@@ -7,6 +7,9 @@ const sdk = new ThirdwebSDK("rinkeby");
 
 const bundleDropModule = sdk.getBundleDropModule(
   "0x7bf15b5b30e0817642063e2765477603Cee99665"
+);
+const tokenModule = sdk.getTokenModule(
+  "0x6bA18655Fd64673C76e6748c6251723ac9C0d405"
 );
 
 const App = () => {
@@ -17,6 +20,54 @@ const App = () => {
 
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   const [isClaiming, setIsClaiming] = useState(false);
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+  };
+
+  useEffect(async () => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    try {
+      const memberAddresses = await bundleDropModule.getAllClaimerAddresses(
+        "0"
+      );
+      setMemberAddresses(memberAddresses);
+      console.log("🚀 Members addresses", memberAddresses);
+    } catch (error) {
+      console.error("failed to get member list", error);
+    }
+  }, [hasClaimedNFT]);
+
+  useEffect(async () => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    try {
+      const amounts = await tokenModule.getAllHolderBalances();
+      setMemberTokenAmounts(amounts);
+      console.log("👜 Amounts", amounts);
+    } catch (error) {
+      console.error("failed to get token amounts", error);
+    }
+  }, [hasClaimedNFT]);
+
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          memberTokenAmounts[address] || 0,
+          18
+        ),
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   useEffect(() => {
     sdk.setProviderOrSigner(signer);
@@ -54,14 +105,38 @@ const App = () => {
     );
   }
 
+
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
         <h1>SolidariDAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
-  };
+  }
 
   const mintNft = async () => {
     setIsClaiming(true);
